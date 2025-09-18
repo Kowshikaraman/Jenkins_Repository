c7a35e58651b371227de5707962e19d5b900219a
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-18 21:15:41 +0530

    new change for git pull

--- pipelines/gitclonerepo.groovy
+++ pipelines/gitclonerepo.groovy
@@ -1,15 +1,25 @@
 def run(String repoUrl, String branch) {
     echo "📥 Cloning ${repoUrl} (branch: ${branch}) into Workspace"
 
-    // Detect OS
-    if (isUnix()) {
-        // Linux / macOS
-        sh "git clone -b ${branch} ${repoUrl} ."
-    } else {
-        // Windows
-        deleteDir()
-        bat "git clone -b ${branch} ${repoUrl} ."
-    }
+    // // Detect OS
+    // if (isUnix()) {
+    //     // Linux / macOS
+    //     sh "git clone -b ${branch} ${repoUrl} ."
+    // } else {
+    //     // Windows
+    //     deleteDir()
+    //     bat "git clone -b ${branch} ${repoUrl} ."
+    // }
+
+        checkout([
+        $class: 'GitSCM',
+        branches: [[name: branch]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [
+            [$class: 'CloneOption', depth: 1, shallow: true, noTags: false, timeout: 10]
+        ],
+        userRemoteConfigs: [[url: repoUrl]]
+    ])
 }
 
 return this
