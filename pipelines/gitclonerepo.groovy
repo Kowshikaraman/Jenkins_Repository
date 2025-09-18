be71f262edb9d9af6c7a1e8acd001d0893da8d25
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-18 21:59:04 +0530

    remove only the folder

--- pipelines/gitclonerepo.groovy
+++ pipelines/gitclonerepo.groovy
@@ -1,6 +1,7 @@
 def run(String repoUrl, String branch, String folder) {
     echo "📥 Cloning ${repoUrl} (branch: ${branch}) into ${folder}"
-    deleteDir()
+    // deleteDir()
+    bat 'rmdir /S /Q ExpressJSApp'
     // Detect OS
     if (isUnix()) {
         // Linux / macOS
