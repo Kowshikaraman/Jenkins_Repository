0e23ea669f1dbcd2f9568a24f8ac6594e347872f
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-18 20:12:54 +0530

    fix for changing the clone repo

--- pipelines/gitclonerepo.groovy
+++ pipelines/gitclonerepo.groovy
@@ -1,13 +1,13 @@
 def run(String repoUrl, String branch, String folder) {
-    echo "📥 Cloning ${repoUrl} (branch: ${branch}) into ${folder}"
+    echo "📥 Cloning ${repoUrl} (branch: ${branch}) into Workspace"
 
     // Detect OS
     if (isUnix()) {
         // Linux / macOS
-        sh "git clone -b ${branch} ${repoUrl} ${folder}"
+        sh "git clone -b ${branch} ${repoUrl} ."
     } else {
         // Windows
-        bat "git clone -b ${branch} ${repoUrl} ${folder}"
+        bat "git clone -b ${branch} ${repoUrl} ."
     }
 }
 
