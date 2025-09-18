669457a372b66d56cb761f8b11bf581557af6b1f
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-18 21:56:28 +0530

    removing files before jenkins clone

--- pipelines/gitclonerepo.groovy
+++ pipelines/gitclonerepo.groovy
@@ -1,6 +1,6 @@
 def run(String repoUrl, String branch, String folder) {
     echo "📥 Cloning ${repoUrl} (branch: ${branch}) into ${folder}"
-
+    deleteDir()
     // Detect OS
     if (isUnix()) {
         // Linux / macOS
