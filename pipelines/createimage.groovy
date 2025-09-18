51ea0356a310d102a5e90c452572334a229a845b
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-18 22:42:55 +0530

    changing the image name to lowercase

--- pipelines/createimage.groovy
+++ pipelines/createimage.groovy
@@ -1,7 +1,7 @@
 def build(){
     def cmdExe = bat """cd ExpressJSApp"""
     dir("ExpressJSApp"){
-        bat """docker build -t ExpressApp:1.0.1 . """
+        bat """docker build -t expressapp:1.0.1 . """
     }
 }
 return this
