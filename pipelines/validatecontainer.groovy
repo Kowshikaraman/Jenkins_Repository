2bf38aee594b433fb494cb3684d4d217c22b3fa7
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-19 14:52:54 +0530

    changing the command to list the container

--- pipelines/validatecontainer.groovy
+++ pipelines/validatecontainer.groovy
@@ -1,5 +1,5 @@
 def validate(String CONTAINER_NAME){
-    def exitStatus1 = bat(script:"docker ps -f --name='${CONTAINER_NAME}'",returnStatus:true)
+    def exitStatus1 = bat(script:"docker ps -aq -f "name=^%CONTAINER_NAME%$"",returnStatus:true)
     if(exitStatus1){
         bat "docker stop ${CONTAINER_NAME} || echo Container already stopped"
         bat "docker rm ${CONTAINER_NAME} || echo Container already Removed"
