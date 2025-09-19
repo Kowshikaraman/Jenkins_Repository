ab85c34a80fce2714ab7a6a25698859c589507a1
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-19 14:57:50 +0530

    test

--- pipelines/validatecontainer.groovy
+++ pipelines/validatecontainer.groovy
@@ -1,5 +1,8 @@
 def validate(String CONTAINER_NAME){
-    def exitStatus1 = bat(script:'docker ps -aq -f "name=^%CONTAINER_NAME%$"',returnStatus:true)
+    def exitStatus1 = bat(
+        script: "docker ps -aq -f \"name=^${CONTAINER_NAME}$\"",
+        returnStdout: true
+    ).trim()
     if(exitStatus1){
         bat "docker stop ${CONTAINER_NAME} || echo Container already stopped"
         bat "docker rm ${CONTAINER_NAME} || echo Container already Removed"
