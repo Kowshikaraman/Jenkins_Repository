cf95bbe2ed982099cbe5789789cc48858e34126b
LAPTOP-GIIANB23\KOWSHIKARAMAN <nba10raman@gmail.com>
2025-09-20 15:53:21 +0530

    fix 8

--- demo-jenkins/demojenkins.groovy
+++ demo-jenkins/demojenkins.groovy
@@ -1,17 +1,3 @@
-// def run() {
-//     node {
-//         stage("Hello World...") {
-//             script {
-//                 def helloWorld = "Welcome!! This is my new message!!!"
-//                 echo "${helloWorld}"
-//             }
-//         }
-//     }
-// }
-
-// return this
-
-
 def run() {
         echo "Welcome!! This is my new message!!!"
 }
